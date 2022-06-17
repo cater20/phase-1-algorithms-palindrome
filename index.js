58751d11ded66bab6d471ab41724c91abd3bced2
cater20 <onyangoketta@gmail.com>
2022-06-17 13:08:49 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  const len=word.length
+  for (i=0; i<len/2 ; i++){
+    if (word[i] === word[len - 1 - i]){
+      return true
+    }else {
+      return false
+    }
+  }
+  }
+
+
 
 /* 
   Add your pseudocode here
+  write ispalindrone function using fuction keywords 
+  pass the string argument
+  if the is a palindrone the function returns true
+  and if function not palindrone it returns false.
+  
 */
 
 /*
   Add written explanation of your solution here
+  what is a palindrone ,
+  A string is a palindrome if it is read the same from forward or backward
 */
 
 // You can run `node index.js` to view these console logs
